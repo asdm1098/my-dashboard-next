8c34bfc33050b3c23b444db55381a7d8a1edd49d
asdm1098 <asdm1098@gmail.com>
2025-01-26 20:26:16 +0100

    fist commit

---
+++ src/pokemons/index.ts
@@ -0,0 +1,7 @@
+export { PokemonCard } from './components/PokemonCard';
+
+export { PokemonGrid } from './components/PokemonGrid';
+
+export type { Pokemon } from './interfaces/pokemon';
+export  type { PokemonsResponse } from './interfaces/pokemons-response';
+export  type { SimplePokemon } from './interfaces/simple-pokemon';
